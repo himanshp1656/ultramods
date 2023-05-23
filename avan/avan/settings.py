c1efc04bb67c0d2b0f4ce8718e1414ce5401c148
himanshp1656 <126962282+himanshp1656@users.noreply.github.com>
2023-05-23 07:12:05 -0700

    Update settings.py

--- avan/avan/settings.py
+++ avan/avan/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6pt$r2s-6mjfeb0tfw$wn(b9(ik#b*x-0)824!4pc(40&z3sm-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
